dab33c3490efd947fd18c064d9731aa06b0677c4
Sylvia <spueyoajona@gmail.com>
2026-08-24 13:13:59 +0200

    Readme actualizado

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main style={{ fontFamily: 'sans-serif', padding: '2rem', textAlign: 'center' }}>
+      <h1>SEN Studio</h1>
+      <p>Diseño y Desarrollo Web a Medida</p>
+    </main>
+  );
+}
