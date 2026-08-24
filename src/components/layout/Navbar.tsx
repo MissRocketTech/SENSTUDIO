244cceeffc04f571fc462be46d2cb4b954adfac2
Sylvia <spueyoajona@gmail.com>
2026-08-24 14:21:43 +0200

    Commit

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,25 @@
+import react frotm 'react';
+import "./Navbar.css";
+
+export const Navbar: React.FC = () => {
+  return (
+    <header className="navbar-container">
+        <div className="navbar-inner">
+            <a href="#hero" className="navbar-logo">
+                <span className="logo-symbol">&lt;s&gt;</span>
+                <span className="logo-text">senstudio.es</span>
+            </a>
+
+            <nav className="navbar-links">
+                <a href="#about" className="navbar-link">Servicios</a>
+                <a href="#projects" className="navbar-link">Ecosistema Phygital</a>
+                <a href="#contact" className="navbar-link">Sobre Sylvia</a>
+            </nav>
+        
+            <a href="#contacto" className="navbar-cta">
+                Hablemos
+            </a>
+        </div>
+       </header>
+  );
+};
